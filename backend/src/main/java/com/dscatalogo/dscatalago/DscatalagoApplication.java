013e93a0098bd4987b1e9629e2e0d2e636b35ba2
bruna juliani <bruna.juliani@inovacaogov.com.br>
2024-09-24 21:37:36 -0300

    bkend project create

---
+++ backend/src/main/java/com/dscatalogo/dscatalago/DscatalagoApplication.java
@@ -0,0 +1,13 @@
+package com.dscatalogo.dscatalago;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DscatalagoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DscatalagoApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/dscatalogo/dscatalago/DscatalagoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dscatalogo.dscatalago;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscatalagoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

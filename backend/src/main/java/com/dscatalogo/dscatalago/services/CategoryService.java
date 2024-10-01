d1620f2c0062ac1d5ff7281854975e2810bd284d
bruna juliani <bruna.juliani@inovacaogov.com.br>
2024-10-01 20:56:45 -0300

    DATABASE SEEDING

--- backend/src/main/java/com/dscatalogo/dscatalago/services/CategoryService.java
+++ backend/src/main/java/com/dscatalogo/dscatalago/services/CategoryService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.dscatalogo.dscatalago.entities.Category;
 import com.dscatalogo.dscatalago.repositories.CategoryRepository;
@@ -15,7 +16,7 @@ public class CategoryService {
 	@Autowired
 	
 	private CategoryRepository repository;
-	
+	@Transactional(readOnly = true)
 	public List<Category> findAll(){
 		
 		return repository.findAll();

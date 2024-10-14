9fdbde6e8922a40884f84fa1d699bcc5bddf4377
bruna juliani <bruna.juliani@inovacaogov.com.br>
2024-10-14 16:07:04 -0300

    Dto

--- backend/src/main/java/com/dscatalogo/dscatalago/resources/CategoryResource.java
+++ backend/src/main/java/com/dscatalogo/dscatalago/resources/CategoryResource.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.dscatalogo.dscatalago.dto.CategoryDTO;
 import com.dscatalogo.dscatalago.entities.Category;
 import com.dscatalogo.dscatalago.services.CategoryService;
 
@@ -18,8 +19,8 @@ public class CategoryResource {
 	private CategoryService service;
 	
     @GetMapping
-	public ResponseEntity<List<Category>> findAll() {
-		List<Category> list = service.findAll();
+	public ResponseEntity<List<CategoryDTO>> findAll() {
+		List<CategoryDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 }
--- backend/src/main/java/com/dscatalogo/dscatalago/services/CategoryService.java
+++ backend/src/main/java/com/dscatalogo/dscatalago/services/CategoryService.java
@@ -1,11 +1,14 @@
 package com.dscatalogo.dscatalago.services;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.dscatalogo.dscatalago.dto.CategoryDTO;
 import com.dscatalogo.dscatalago.entities.Category;
 import com.dscatalogo.dscatalago.repositories.CategoryRepository;
 
@@ -17,10 +20,13 @@ public class CategoryService {
 	
 	private CategoryRepository repository;
 	@Transactional(readOnly = true)
-	public List<Category> findAll(){
+	public List<CategoryDTO> findAll(){
 		
-		return repository.findAll();
+		List<Category> list = repository.findAll();
 		
-
+		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+		
+		
+		
 }
 }
